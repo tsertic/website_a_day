286625c5f7fb2d8be1c1d93070625d6251a2325f
Tomislav Sertic <tsertic5@gmail.com>
2023-07-31 23:11:16 +0200

    project7: added drag and drop logic for stores

--- project7_shopping_list_dnd/app/page.tsx
+++ project7_shopping_list_dnd/app/page.tsx
@@ -1,5 +1,69 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
+import DATA from "./../data/shoppingCart.json";
+import {
+  Draggable,
+  Droppable,
+  DragDropContext,
+  DropResult,
+} from "react-beautiful-dnd";
 export default function Home() {
-  return <main className=" p-24"></main>;
+  const [stores, setStores] = useState(DATA);
+  const handleOnDragEnd = (results: DropResult) => {
+    console.log(results);
+
+    const { source, type, destination } = results;
+
+    if (!destination) return;
+
+    const sourceId = source.droppableId;
+    const destinationId = destination.droppableId;
+    const sourceIndex = source.index;
+    const destinationIndex = destination.index;
+
+    if (sourceId === destinationId && sourceIndex === destinationIndex) return;
+
+    if (type === "stores") {
+      const reorderedStores = [...stores];
+      const [removedStore] = reorderedStores.splice(sourceIndex, 1);
+      reorderedStores.splice(destinationIndex, 0, removedStore);
+      return setStores(reorderedStores);
+    }
+  };
+  return (
+    <main className="bg-white text-gray-800 rounded-xl w-[500px] p-3 mt-10">
+      <h1 className="text-2xl text-center mb-5">Shopping List</h1>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="ROOT" type="stores">
+          {(providerDrop) => {
+            return (
+              <div {...providerDrop.droppableProps} ref={providerDrop.innerRef}>
+                {stores.map((store, i) => {
+                  return (
+                    <Draggable draggableId={store.id} index={i} key={store.id}>
+                      {(providerDrag) => {
+                        return (
+                          <div
+                            className=" "
+                            {...providerDrag.dragHandleProps}
+                            {...providerDrag.draggableProps}
+                            ref={providerDrag.innerRef}
+                          >
+                            <h3 className=" rounded-xl text-center bg-gray-300 mb-2">
+                              {store.name}
+                            </h3>
+                          </div>
+                        );
+                      }}
+                    </Draggable>
+                  );
+                })}
+                {providerDrop.placeholder}
+              </div>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
+    </main>
+  );
 }
